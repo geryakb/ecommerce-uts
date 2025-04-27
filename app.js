d85099bb09fb039ae34873029c880489e010c98f
geryakb <167272303+geryakb@users.noreply.github.com>
2025-04-27 21:23:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-const imageUrls = [
-    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu1.jpg',
-    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu2.jpg',
-    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu3.jpg'
-];
+// Allow frontend to access backend
+app.use(cors());
 
+// MySQL connection
 const db = mysql.createConnection({
     host: 'database-3.c58wagsk8kyu.ap-southeast-1.rds.amazonaws.com',
     user: 'admin',
@@ -17,21 +16,29 @@ const db = mysql.createConnection({
     database: 'ecommerce'
 });
 
-app.get('/', (req, res) => {
+// Produk API endpoint
+app.get('/api/produk', (req, res) => {
     db.query('SELECT price FROM products LIMIT 1', (err, results) => {
-        if (err) throw err;
-        const price = results[0].price;
+        if (err) {
+            console.error('Database error:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
 
-        const imagesHtml = imageUrls.map(url => `<img src="${url}" width="200" style="margin:10px;" />`).join('');
+        const price = results[0]?.price || 0;
+        const imageUrls = [
+            'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu1.jpg',
+            'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu2.jpg',
+            'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu3.jpg'
+        ];
 
-        res.send(`
-            <h1>Produk Kami</h1>
-            ${imagesHtml}
-            <p>Harga: Rp ${price}</p>
-        `); // <-- jangan lupa tutup disini
+        res.json({
+            images: imageUrls,
+            price: price
+        });
     });
 });
 
+// Start server
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server berjalan di http://0.0.0.0:${port}`);
+    console.log(`Backend berjalan di http://0.0.0.0:${port}`);
 });
