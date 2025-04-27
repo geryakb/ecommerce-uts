e552b3f755c7a02b4db32a3ce6a3bb7c63f718dd
geryakb <167272303+geryakb@users.noreply.github.com>
2025-04-27 21:13:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-// Daftar URL gambar langsung
 const imageUrls = [
     'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu1.jpg',
     'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu2.jpg',
     'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu3.jpg'
 ];
 
-// MySQL
 const db = mysql.createConnection({
     host: 'database-3.c58wagsk8kyu.ap-southeast-1.rds.amazonaws.com',
     user: 'admin',
@@ -19,18 +17,18 @@ const db = mysql.createConnection({
     database: 'ecommerce'
 });
 
-// Route utama
 app.get('/', (req, res) => {
     db.query('SELECT price FROM products LIMIT 1', (err, results) => {
         if (err) throw err;
         const price = results[0].price;
 
-        const imagesHtml = imageUrls.map(url => `<img src="${url}" width="200" style="margin:10px;" />
+        const imagesHtml = imageUrls.map(url => `<img src="${url}" width="200" style="margin:10px;" />`).join('');
+
         res.send(`
             <h1>Produk Kami</h1>
             ${imagesHtml}
             <p>Harga: Rp ${price}</p>
-        `);
+        `); // <-- jangan lupa tutup disini
     });
 });
 
