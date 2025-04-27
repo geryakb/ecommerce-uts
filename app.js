c9f644b006e6d12b878b74d85923c3792fbee73a
geryakb <167272303+geryakb@users.noreply.github.com>
2025-04-27 21:02:21 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// Daftar URL gambar langsung
+const imageUrls = [
+    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu1.jpg',
+    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu2.jpg',
+    'https://ecommerce-gambar-produk.s3.ap-southeast-1.amazonaws.com/sepatu3.jpg'
+];
+
+// MySQL
+const db = mysql.createConnection({
+    host: 'database-3.c58wagsk8kyu.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'katasandi1',
+    database: 'ecommerce'
+});
+
+// Route utama
+app.get('/', (req, res) => {
+    db.query('SELECT price FROM products LIMIT 1', (err, results) => {
+        if (err) throw err;
+        const price = results[0].price;
+
+        const imagesHtml = imageUrls.map(url => `<img src="${url}" width="200" style="margin:10px;" />
+        res.send(`
+            <h1>Produk Kami</h1>
+            ${imagesHtml}
+            <p>Harga: Rp ${price}</p>
+        `);
+    });
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server berjalan di http://0.0.0.0:${port}`);
+});
